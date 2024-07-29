c57eae77c5832d3731156be48ec6d66c3ff192af
rose-sky-sun <rose.sky000801@gmail.com>
2024-07-29 22:54:28 +0200

    added js file. building js on search section of the app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput =document.querySelector("#search-bar");
+}
+
+let searchElement = document.querySelector("#search-form");
+searchElement.addEventListener("submit", handleSearchSubmit);
