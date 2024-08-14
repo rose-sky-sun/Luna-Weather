010f0532234898e5fee077fff6531d9606821323
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-08-14 18:05:17 +0200

    updated js to show weather description frrom API

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temp-value");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+
+    console.log(response.data.condition.description);
 
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
