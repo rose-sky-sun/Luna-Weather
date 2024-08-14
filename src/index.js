c3842d27a9f07f614b9b2d092942153ccbddf368
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-08-14 17:30:24 +0200

    updated js to round temperature value

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temp-value");
-    temperatureElement.innerHTML = response.data.temperature.current;
+    let temperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(temperature);
 }
 
 
