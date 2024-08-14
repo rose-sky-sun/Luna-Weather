2707c105bfe8c7dca2eb03ac23cff1be6ab458b8
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-08-14 18:20:53 +0200

    update js to show weather humidity from API

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ function refreshWeather(response) {
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#current-city");
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
 
-    console.log(response.data.condition.description);
+    console.log(response.data.temperature.humidity);
 
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
