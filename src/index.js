3d2eef9d3c7cdb16c0508ae703a88d21b8e0fb21
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-08-14 19:11:10 +0200

    updated js to show a better format for minutes less than 10

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ function refreshWeather(response) {
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
 
-    console.log(response.data);
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
@@ -33,6 +32,10 @@ function formatDate(date) {
     ];
     let day = days[date.getDay()];
 
+    //if minutes is less than 10 -- fix the format for time
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
 
     return `${day} ${hours}:${minutes}`;
     
