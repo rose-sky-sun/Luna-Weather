772a11cbb5ad9c6657507ee3555d151afbdee57e
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-08-15 17:33:27 +0200

    replaced hard coded icon from api to change in real time based on user search

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ function refreshWeather(response) {
     let windElement = document.querySelector("#wind");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
-
+    
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    iconElement.innerHTML = `<img id="temp-icon" src="${response.data.condition.icon_url}">`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
