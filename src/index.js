35306e8d5251e6fb4d4fda32095d18c54acb24eb
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-08-16 21:08:30 +0200

    replaced hardcoded html dummy data to be inserted via JS for better maintainability

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-   let apiKey = "b2aSadcct04b33178913oc33Sf40S433"
+   let apiKey = "2a48ab03df66cfdeo2cf2td262aab2a0"
    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
@@ -58,7 +58,36 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+//function to call for displaying the forecast
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let days = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed']; // dummy data before integrating API
+    let forecastHtml = "";
+
+    days.forEach(function(day) {
+        forecastHtml =
+            forecastHtml +
+         `
+            <div class="daily-forecast" >
+                <div class="forecast-day">${day}</div>
+                <div class="forecast-icon">🌤️</div>
+                <div class="forecast-temps">
+                    <div class="forecast-temp"><strong>15°</strong></div>
+                    <div class="forecast-temp">9°</div>
+                </div>
+            </div>
+        `;
+    });
+
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Oslo");
+displayForecast();
+
+
+
